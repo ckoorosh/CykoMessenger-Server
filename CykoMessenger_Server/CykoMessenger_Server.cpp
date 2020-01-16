425a6120a4153f93e803eb693a6e8693579ac0a8
ckoorosh <53394330+ckoorosh@users.noreply.github.com>
2020-01-16 22:48:04 +0330

    send message+refresh

--- CykoMessenger_Server/CykoMessenger_Server.cpp
+++ CykoMessenger_Server/CykoMessenger_Server.cpp
@@ -1,4 +1,5 @@
 #include "stdafx.h"
+#include "Functions.c"
 #include <winsock2.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,13 +7,82 @@
 #include <Windows.h>
 #include <conio.h>
 #include "cJSON.h"
-#include "Functions.c"
 #include <string.h>
 #pragma comment(lib,"WS2_32")
 
 int main()
 {
-	socket_connect();
+
+		WORD wVersionRequested;
+		WSADATA wsaData;
+		int err;
+
+		// Use the MAKEWORD(lowbyte, highbyte) macro declared in Windef.h
+		wVersionRequested = MAKEWORD(2, 2);
+
+		err = WSAStartup(wVersionRequested, &wsaData);
+		if (err != 0) {
+			// Tell the user that we could not find a usable Winsock DLL.
+			printf("WSAStartup failed with error: %d\n", err);
+			return 0;
+		}
+
+		// Create and verify socket
+		server_socket = socket(AF_INET, SOCK_STREAM, 6);
+		if (server_socket == INVALID_SOCKET)
+			wprintf(L"socket function failed with error = %d\n", WSAGetLastError());
+		else
+			printf("Socket successfully created...\n");
+
+		// Assign IP and port
+		memset(&server, 0, sizeof(server));
+		server.sin_family = AF_INET;
+		server.sin_addr.s_addr = htonl(INADDR_ANY);
+		server.sin_port = htons(PORT);
+
+		// Bind newly created socket to given IP and verify
+		if ((bind(server_socket, (SA *)&server, sizeof(server))) != 0)
+		{
+			printf("Socket binding failed...\n");
+			exit(0);
+		}
+		else
+			printf("Socket successfully bound...\n");
+
+		// Now server is ready to listen and verify
+		if ((listen(server_socket, 5)) != 0)
+		{
+			printf("Listen failed...\n");
+			exit(0);
+		}
+		else
+			printf("Server listening..\n");
+
+		int flag = 0;
+		while (1)
+		{
+			// Accept the data packet from client and verify
+			int len = sizeof(client);
+			client_socket = accept(server_socket, (SA *)&client, &len);
+			if (client_socket < 0)
+			{
+				printf("Server accceptance failed...\n");
+				exit(0);
+			}
+			else
+				if (!flag) {
+					flag = 1;
+					printf("Server acccepted the client..\n");
+				}
+
+			// Function for chatting between client and server
+			recieve();
+
+		}
+
+		// Close the socket
+		closesocket(server_socket);
+
     return 0;
 }
 
--- CykoMessenger_Server/Functions.c
+++ CykoMessenger_Server/Functions.c
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include <winsock2.h>
 #include <stdio.h>
+#include <time.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <Windows.h>
@@ -18,16 +19,20 @@ void socket_connect();
 void order_finder(char* buffer);
 void create_account();
 void signin();
-//void create_channel();
-//void join_channel();
+char* token_generator();
+void create_channel();
+void join_channel();
+void send_message();
 //void logout();
-//void send_message();
-//void refresh();
+void refresh();
 //void channel_members();
 //void leave_channel();
 
 
 int server_socket, client_socket;
+int channel_id=0, user_id=0;
+char token[100];
+char *messgs;
 struct sockaddr_in server, client;
 
 
@@ -39,79 +44,15 @@ typedef struct use
 }USER;
 USER user;
 
-void socket_connect()
+typedef struct chann
 {
-	
-	WORD wVersionRequested;
-	WSADATA wsaData;
-	int err;
+	char name[100];
+	char message[100];
+	int id;
+}CHANNEL;
+CHANNEL channel;
 
-	// Use the MAKEWORD(lowbyte, highbyte) macro declared in Windef.h
-	wVersionRequested = MAKEWORD(2, 2);
 
-	err = WSAStartup(wVersionRequested, &wsaData);
-	if (err != 0) {
-		// Tell the user that we could not find a usable Winsock DLL.
-		printf("WSAStartup failed with error: %d\n", err);
-		return;
-	}
-
-	// Create and verify socket
-	server_socket = socket(AF_INET, SOCK_STREAM, 6);
-	if (server_socket == INVALID_SOCKET)
-		wprintf(L"socket function failed with error = %d\n", WSAGetLastError());
-	else
-		printf("Socket successfully created...\n");
-
-	// Assign IP and port
-	memset(&server, 0, sizeof(server));
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = htonl(INADDR_ANY);
-	server.sin_port = htons(PORT);
-
-	// Bind newly created socket to given IP and verify
-	if ((bind(server_socket, (SA *)&server, sizeof(server))) != 0)
-	{
-		printf("Socket binding failed...\n");
-		exit(0);
-	}
-	else
-		printf("Socket successfully bound...\n");
-
-	// Now server is ready to listen and verify
-	if ((listen(server_socket, 5)) != 0)
-	{
-		printf("Listen failed...\n");
-		exit(0);
-	}
-	else
-		printf("Server listening..\n");
-
-	int flag = 0;
-	while (1)
-	{
-		// Accept the data packet from client and verify
-		int len = sizeof(client);
-		client_socket = accept(server_socket, (SA *)&client, &len);
-		if (client_socket < 0)
-		{
-			printf("Server accceptance failed...\n");
-			exit(0);
-		}
-		else
-			if (!flag) {
-				flag = 1;
-				printf("Server acccepted the client..\n");
-			}
-
-		// Function for chatting between client and server
-		recieve();
-
-	}
-
-	// Close the socket
-	closesocket(server_socket);
-}
 
 
 
@@ -153,15 +94,26 @@ void order_finder(char* buffer) {
 		sscanf(buffer, "%*s %[^',']%*c%s", user.username, user.password);
 		create_account();
 	}
-	if (strncmp(buffer, "login", 5) == 0) {
+	else if (strncmp(buffer, "login", 5) == 0) {
 		sscanf(buffer, "%*s %[^',']%*c%s", user.username, user.password);
 		signin();
 	}
-	/*if (strncmp(buffer, "create channel", 14) == 0) return 3;
-	if (strncmp(buffer, "join channel", 12) == 0)return 4;
-	if (strncmp(buffer, "send", 4) == 0)return 5;
-	if (strncmp(buffer, "Refresh", 7) == 0)return 6;
-	if (strncmp(buffer, "channel members", 15) == 0)return 7;
+	else if (strncmp(buffer, "create channel", 14) == 0) {
+		sscanf(buffer, "%*s%*s %[^',']%*c%s", channel.name, token);
+		create_channel();
+	}
+	else if (strncmp(buffer, "join channel", 12) == 0) {
+		sscanf(buffer, "%*s%*s %[^',']%*c%s", channel.name, token);
+		join_channel();
+	}
+	else if (strncmp(buffer, "send", 4) == 0) {
+		sscanf(buffer, "%*s %[^',']%*c%s", channel.message, token);
+		send_message();
+	}
+	if (strncmp(buffer, "Refresh", 7) == 0) {
+		sscanf(buffer, "%*s %s",  token);
+	}
+	/*if (strncmp(buffer, "channel members", 15) == 0)return 7;
 	if (strncmp(buffer, "leave", 5) == 0)return 8;
 	if (strncmp(buffer, "logout", 6) == 0)return 9;*/
 }
@@ -180,11 +132,11 @@ void create_account() {  //----------------------------------- CREATE ACCOUNT --
 	else {	// file doesn't exist
 		sprintf(buffer, "{\"type\": \"Successful\", \"content\": \"\"}");
 		userfile = fopen(filename, "w");
+		// Saving in file
+		fprintf(userfile, "{\"username\":\"%s\",\"password\":\"%s\"}", user.username, user.password);
+		fclose(userfile);
+		user_id++;
 	}
-	// Saving in file
-	fprintf(userfile, "{\"username\":\"%s\",\"password\":\"%s\"}", user.username, user.password);
-	fclose(userfile);
-
 	// Send the buffer to client
 	send(client_socket, buffer, sizeof(buffer), 0);
 	
@@ -204,14 +156,16 @@ void signin() {
 		fgets(js,100,userfile);
 		cJSON* out = cJSON_Parse(js);
 		char *password = cJSON_GetObjectItem(out, "password")->valuestring;
-		if (strcmp(password, user.password) == 0)
-			sprintf(buffer, "{\"type\": \"Successful\", \"content\": \"\"}");
+		if (strcmp(password, user.password) == 0) {
+			strcpy(user.token , token_generator());
+			sprintf(buffer, "{\"type\": \"AuthToken\", \"content\": \"%s\"}",user.token );
+		}
 		else
 			sprintf(buffer, "{\"type\": \"Error\", \"content\": \"Password is not correct!\"}");
 	}
 
 	else 	// file doesn't exist
-		sprintf(buffer, "{\"type\": \"Error\", \"content\": \"User doesnt exists!\"}");
+		sprintf(buffer, "{\"type\": \"Error\", \"content\": \"User does not exist!\"}");
 	// Send the buffer to client
 	send(client_socket, buffer, sizeof(buffer), 0);
 
@@ -219,3 +173,160 @@ void signin() {
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+char* token_generator() {
+	srand(time(NULL));
+	char *string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	int n = 16;
+	char tkn[17];
+	for (int i = 0; i < n; i++)
+		tkn[i]=string[rand() % 62 ];
+	tkn[16] = '\0';
+	return tkn;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void create_channel() {
+
+	char buffer[MAX];
+	if (strcmp(token, user.token) != 0) sprintf(buffer, "{\"type\": \"Error\", \"content\": \"Authentication failed!\"}");
+	else {
+		char filename[100];
+		sprintf(filename, "./Resources/Channels/%s.cyko", channel.name);
+		if (access(filename, 0) != -1) {    // file exists		
+			sprintf(buffer, "{\"type\": \"Error\", \"content\": \"Channel already exists!\"}");
+		}
+
+		else { 	// file doesn't exist
+			FILE* channelfile;
+			channelfile = fopen(filename, "w");
+			fprintf(channelfile, "{\"messages\":[{\"sender\":\"server\",\"content\":\"%s created %s.\"}],\"name\":\"%s\"\"}", user.username, channel.name,user.username);
+			fclose(channelfile);
+			sprintf(buffer, "{\"type\": \"Successful\", \"content\": \"\"}");
+		}
+		// Send the buffer to client
+		send(client_socket, buffer, sizeof(buffer), 0);
+
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void join_channel() {
+
+	char buffer[MAX];
+	if (strcmp(token, user.token) != 0) 	sprintf(buffer, "{\"type\": \"Error\", \"content\": \"Authentication failed!\"}");
+	else {
+		FILE* channelfile;
+		char filename[100];
+		sprintf(filename, "./Resources/Channels/%s.cyko", channel.name);
+		if (access(filename, 0) != -1) {	// file exists
+			sprintf(buffer, "{\"type\": \"Successful\", \"content\": \"\"}");
+			channelfile = fopen(filename, "w");
+
+			/************************* Updting the messages ****************************/
+			puts("1");
+			char js[10000];
+			fgets(js, 10000, channelfile);
+			strcpy(messgs, js);
+			printf("%s\n%s", messgs, js);
+			cJSON *msg_array, *msg_array2, *item;
+			cJSON *messages = cJSON_Parse(js);
+			cJSON *message = cJSON_Parse(messgs);
+			if (!messages||!message) {
+				printf("Error before: [%s]\n", cJSON_GetErrorPtr());
+			}
+			msg_array = cJSON_GetObjectItem(messages, "messages");
+			msg_array2 = cJSON_GetObjectItem(message, "messages");
+
+			// Create a new array item and add sender and message
+			item = cJSON_CreateObject();
+			cJSON_AddItemToObject(item, "sender", cJSON_CreateString("server"));
+			char buf[100];
+			sprintf(buf, "%s created channel %s", user.username, channel.name);
+			cJSON_AddItemToObject(item, "message", cJSON_CreateString(buf));
+
+			// insert the new message into the existing array
+			cJSON_AddItemToArray(msg_array, item);
+
+			printf("%s\n", cJSON_Print(messages));
+			cJSON_Delete(messages);
+
+			/***************************************************************/
+			puts("1");
+			// Create a new array item and add sender and message
+			item = cJSON_CreateObject();
+			cJSON_AddItemToObject(item, "sender", cJSON_CreateString("server"));
+			sprintf(buf, "%s created channel %s", user.username, channel.name);
+			cJSON_AddItemToObject(item, "message", cJSON_CreateString(buf));
+
+			// insert the new message into the existing array
+			cJSON_AddItemToArray(msg_array2, item);
+
+			printf("%s\n", cJSON_Print(message));
+			cJSON_Delete(message);
+
+			/************************************************************************************/
+			puts("1");
+			fprintf(channelfile, js);
+
+			// Closing the file
+			fclose(channelfile);
+		}
+		else {	// file doesn't exist
+			sprintf(buffer, "{\"type\": \"Error\", \"content\": \"Channel does not exist!\"}");
+		}
+	}
+	printf("%s", buffer);
+	// Send the buffer to client
+	send(client_socket, buffer, sizeof(buffer), 0);
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void send_message() {
+
+	char buffer[MAX];
+	if (strcmp(token, user.token) != 0) 	sprintf(buffer, "{\"type\": \"Error\", \"content\": \"Authentication failed!\"}");
+	else {
+		sprintf(buffer, "{\"type\": \"Successful\", \"content\": \"\"}");
+		cJSON *msg_array, *item;
+		cJSON *messages = cJSON_Parse(messgs);
+		if (!messages) {
+			printf("Error before: [%s]\n", cJSON_GetErrorPtr());
+		}
+		msg_array = cJSON_GetObjectItem(messages, "messages");
+
+		// Create a new array item and add sender and message
+		item = cJSON_CreateObject();
+		cJSON_AddItemToObject(item, "sender", cJSON_CreateString(user.username));
+		cJSON_AddItemToObject(item, "message", cJSON_CreateString(channel.message));
+
+		// insert the new message into the existing array
+		cJSON_AddItemToArray(msg_array, item);
+
+		printf("%s\n", cJSON_Print(messages));
+		cJSON_Delete(messages);
+	}
+
+	// Send the buffer to client
+	send(client_socket, buffer, sizeof(buffer), 0);
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void refresh() {
+	char* mssg="";
+	char filename[100];
+	sprintf(filename, "./Resources/Channels/%s.cyko", channel.name);
+	
+	cJSON* out = cJSON_Parse(messgs);
+	mssg = cJSON_GetObjectItem(out, "messages")->valuestring;
+	sprintf(mssg, "\"type\": \"List\",\"content\":%s", mssg);
+
+	// Send the buffer to client
+	send(client_socket,mssg, sizeof(mssg), 0);
+	messgs = "";
+}
